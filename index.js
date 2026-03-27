c5acf09b1ae861cf731cc85ed9c6d401745fde1d
Harkirat <harkirat96@gmail.com>
2026-03-27 21:53:45 +0530

    migrated signup and signin

--- index.js
+++ index.js
@@ -1,43 +1,45 @@
 const express = require("express");
 const { authMiddleware } = require("./middleware");
 const jwt = require("jsonwebtoken");
+const { todoModel, userModel } = require("./models");
+
 const app = express()
 app.use(express.json());
 
 
-let CURRENT_USER_ID = 1;
-let CURRENT_TODO_ID = 1;
-
-let USERS = [];
-let TODOS = [];
-
-app.post("/signup", (req, res) => {
+app.post("/signup", async (req, res) => {
     const username = req.body.username;
     const password = req.body.password;
 
-    const existingUser = USERS.find(u => u.username === username);
+    const existingUser = await userModel.findOne({
+        username: username,
+        password: password
+    });
     if (existingUser) {
         res.status(403).json({
             message: "User with this username already exists"
         })
         return 
     }
-    USERS.push({
-        id: CURRENT_USER_ID++,
-        username,
-        password
+   
+    const newUser = await userModel.create({
+        username: username,
+        password: password
     })
     res.json({
-        id: CURRENT_USER_ID - 1
+        id: newUser._id
     })
 })
 
 
-app.post("/signin", (req, res) => {
+app.post("/signin", async (req, res) => {
     const username = req.body.username;
     const password = req.body.password;
 
-    const userExists = USERS.find(u => u.username === username && u.password === password);
+    const userExists = await userModel.findOne({
+        username: username,
+        password: password
+    });
     if (!userExists) {
         res.status(403).json({
             message: "Incorrect credentials"
@@ -53,6 +55,7 @@ app.post("/signin", (req, res) => {
     })
 })
 
+//TODO: Finish all the endpoints from here, migrate them from in memory to mongodb
 app.post("/todo", authMiddleware, (req, res) => {
     const userId = req.userId;
     const title = req.body.title;
